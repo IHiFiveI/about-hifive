2aeb5b51a624f2d37539911aabafb82286bcac35
Nikita <57842399+IHiFiveI@users.noreply.github.com>
2025-10-12 16:25:18 +0700

    feature(navbar): Adjusted gap value;

--- src/ui/layout.tsx
+++ src/ui/layout.tsx
@@ -3,7 +3,7 @@ import { Navbar } from './navbar';
 
 export const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="m-auto flex h-full w-[min(100%,_1080px)] flex-col gap-8">
+    <div className="m-auto flex h-full w-[min(100%,_1080px)] flex-col gap-4">
       <Navbar />
       <div>{children}</div>
     </div>
