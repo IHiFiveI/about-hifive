57dbd7732d95985ca83ef9440e2f6d4cb8d6c953
Nikita <57842399+IHiFiveI@users.noreply.github.com>
2025-10-26 01:30:01 +0700

    feature(footer): Created the footer component; feature(footer): Adjusted layout styles to allow full-width footer;

---
+++ src/ui/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-secondary text-secondary-foreground flex justify-center p-4">
+      © 2025-present
+    </footer>
+  );
+};
--- src/ui/layout.tsx
+++ src/ui/layout.tsx
@@ -1,13 +1,17 @@
+import { Footer } from './footer';
 import { Navbar } from './navbar';
 import { Outlet } from 'react-router';
 
 export const Layout = () => {
   return (
-    <div className="m-auto flex h-full w-[min(100%,_1080px)] flex-col gap-4">
-      <Navbar />
-      <div>
-        <Outlet />
+    <div className="flex h-full flex-col justify-between">
+      <div className="mx-auto flex w-[min(100%,_1080px)] flex-col gap-4">
+        <Navbar />
+        <div className="size-full min-h-[720px]">
+          <Outlet />
+        </div>
       </div>
+      <Footer />
     </div>
   );
 };
