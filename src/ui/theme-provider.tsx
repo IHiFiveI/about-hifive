67d4d50bd16f7b61c5b0e8aabe1261c63103792f
Nikita <57842399+IHiFiveI@users.noreply.github.com>
2025-10-26 11:28:51 +0700

    feature(theme-provider): Added automatic theme change when system theme changes;

--- src/ui/theme-provider.tsx
+++ src/ui/theme-provider.tsx
@@ -37,13 +37,23 @@ export function ThemeProvider({
     root.classList.remove('light', 'dark');
 
     if (theme === 'system') {
-      const systemTheme = window.matchMedia('(prefers-color-scheme: dark)')
-        .matches
-        ? 'dark'
-        : 'light';
+      const colorSchemeMedia = window.matchMedia(
+        '(prefers-color-scheme: dark)'
+      );
 
-      root.classList.add(systemTheme);
-      return;
+      const onChange = () => {
+        root.classList.remove('light', 'dark');
+
+        const systemTheme = colorSchemeMedia.matches ? 'dark' : 'light';
+
+        root.classList.add(systemTheme);
+      };
+
+      colorSchemeMedia.addEventListener('change', onChange);
+
+      return () => {
+        colorSchemeMedia.removeEventListener('change', onChange);
+      };
     }
 
     root.classList.add(theme);
