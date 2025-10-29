6c3c22b9ddfde946ad6856a19ebff6a71dfb166a
Nikita <57842399+IHiFiveI@users.noreply.github.com>
2025-10-30 00:51:17 +0700

    feature(home page): Created separate sections for info about me and about the site itself;

---
+++ src/pages/home/ui/about-me-section.tsx
@@ -0,0 +1,22 @@
+export const AboutMeSection = () => {
+  return (
+    <section>
+      <div className="mb-4 mt-16 flex flex-wrap justify-between sm:flex-nowrap">
+        <h2 className="text-4xl">Greetings!! My name is Nikíta.</h2>
+        <time className="text-accent-foreground/60 self-end">29.10.2025</time>
+      </div>
+      <p className="text-xl">
+        Im a React frontend developer with 4+ years of experience.
+        <br />
+        <br />I strive to write clean, maintainable code and build user-friendly
+        interfaces that solve real problems.
+      </p>
+      {/* TODO: Create grid with info about me: */}
+      {/* my hobbies */}
+      {/* my skills (a couple of main ones and a link to the skills page) */}
+      {/* what genres i like */}
+      {/* current location: UTC+7 */}
+      {/* People Im inspired by: Matt Pocock & Josh Comeau*/}
+    </section>
+  );
+};
--- src/pages/home/ui/home.tsx
+++ src/pages/home/ui/home.tsx
@@ -1,28 +1,18 @@
 import Marquee from 'react-fast-marquee';
 import { GREETINGS } from '../lib/greetings';
-import { AboutMeCard } from './about-me-card';
-import { SiteReasoningCollapse } from './site-reasoning-collapse';
-import { Anchor } from '@/ui/anchor';
+import { AboutMeSection } from './about-me-section';
+import { AboutThisSiteSection } from './about-this-site-section';
 
 export const Home = () => {
   return (
     <>
+      {/* FIXME: Make `aria-hidden="true"` work properly */}
       <Marquee className="text-primary flex scroll-m-20 gap-4 overflow-hidden text-balance text-center text-4xl font-extrabold tracking-tight [&_.rfm-child]:pr-4">
         {GREETINGS}
       </Marquee>
       <div className="flex flex-col gap-8 p-8">
-        <SiteReasoningCollapse />
-        <AboutMeCard />
-        <span>
-          The theme was taken from:{' '}
-          <Anchor
-            target="_blank"
-            href="https://tweakcn.com/"
-            className="text-primary underline"
-          >
-            https://tweakcn.com/
-          </Anchor>
-        </span>
+        <AboutMeSection />
+        <AboutThisSiteSection />
       </div>
     </>
   );
