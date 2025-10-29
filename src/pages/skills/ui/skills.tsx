5e40692d86d262d1585ab5bd44f2c7e1222da299
Nikita <57842399+IHiFiveI@users.noreply.github.com>
2025-10-30 01:56:07 +0700

    feature(skills): Added comment;

--- src/pages/skills/ui/skills.tsx
+++ src/pages/skills/ui/skills.tsx
@@ -1,6 +1,7 @@
 export const Skills = () => {
   return (
     <div className="flex flex-col p-8 pt-4">
+      {/* TODO: Look into creating a background image with https://tinkersynth.com/ */}
       <h3>coming soon!</h3>
       <br />
       i could add another bullet list here but it would be too boring
