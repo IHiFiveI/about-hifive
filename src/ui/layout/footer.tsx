6d8d80fee09e91f8463ffd9de320729d0baa9714
Nikita <57842399+IHiFiveI@users.noreply.github.com>
2025-10-29 21:35:24 +0700

    feature(footer): Added popover for the hh option;

--- src/ui/layout/footer.tsx
+++ src/ui/layout/footer.tsx
@@ -2,13 +2,14 @@ import { RestrictedWidthWrapper } from './restricted-width-wrapper';
 import { Github } from '../icons/github';
 import { HeadHunter } from '../icons/head-hunter';
 import { LinkedIn } from '../icons/linkedin';
+import { Popover, PopoverContent, PopoverTrigger } from '../popover';
 
 export const Footer = () => {
   return (
     <footer className="bg-secondary text-secondary-foreground/80 flex justify-center p-4">
       <RestrictedWidthWrapper className="flex-row items-center justify-between">
         <span>© 2025-present</span>
-        <ul className="flex gap-2">
+        <ul className="flex gap-4 sm:gap-2">
           <li>
             <a
               title="Github"
@@ -19,7 +20,15 @@ export const Footer = () => {
             </a>
           </li>
           <li>
-            <HeadHunter className="size-8" />
+            <Popover>
+              <PopoverTrigger>
+                <HeadHunter className="size-8 cursor-pointer" />
+              </PopoverTrigger>
+              <PopoverContent>
+                if you really are interested in my resume, better contact me by
+                email\linkedin or with any other methods !
+              </PopoverContent>
+            </Popover>
           </li>
           <li>
             <a
