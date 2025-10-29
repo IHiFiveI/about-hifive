fb9955f56639707f0fbab014996dbcee923fa052
Nikita <57842399+IHiFiveI@users.noreply.github.com>
2025-10-30 01:32:40 +0700

    fix(home): Removed purely visual greetings from the accessability tree;

--- src/pages/home/ui/home.tsx
+++ src/pages/home/ui/home.tsx
@@ -1,15 +1,11 @@
-import Marquee from 'react-fast-marquee';
-import { GREETINGS } from '../lib/greetings';
 import { AboutMeSection } from './about-me-section';
 import { AboutThisSiteSection } from './about-this-site-section';
+import { Greetings } from './greetings';
 
 export const Home = () => {
   return (
     <>
-      {/* FIXME: Make `aria-hidden="true"` work properly */}
-      <Marquee className="text-primary flex scroll-m-20 gap-4 overflow-hidden text-balance text-center text-4xl font-extrabold tracking-tight [&_.rfm-child]:pr-4">
-        {GREETINGS}
-      </Marquee>
+      <Greetings />
       <div className="flex flex-col gap-8 p-8">
         <AboutMeSection />
         <AboutThisSiteSection />
