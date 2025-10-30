3b4fef225fa5ef4d153cd0f36f46d977a0dc969d
Nikita <57842399+IHiFiveI@users.noreply.github.com>
2025-10-30 11:50:12 +0700

    feature(skills): Added temporary bullet list grid with skills;

---
+++ src/pages/skills/ui/skill-grid-cell.tsx
@@ -0,0 +1,25 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/ui/card';
+
+interface SkillGridCellProps {
+  title: string;
+  skillNames: string[];
+}
+
+export const SkillGridCell = ({ title, skillNames }: SkillGridCellProps) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>
+          <h3 className="my-4 text-3xl">{title}</h3>
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <ul className="[&_li]:list-inside [&_li]:list-disc">
+          {skillNames.map((name) => (
+            <li key={name}>{name}</li>
+          ))}
+        </ul>
+      </CardContent>
+    </Card>
+  );
+};
